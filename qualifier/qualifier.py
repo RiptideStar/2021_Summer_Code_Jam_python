1db3998df751559d3fc012061def680e360d7804
Kyle Z <kyle1001001@gmail.com>
2021-06-29 23:09:21 -0700

    v1.1

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,12 +1,24 @@
 from typing import Any, List, Optional
 
+VERTICAL = "│"
+HORIZONTAL = "─"
+TOP_LEFT = "┌"
+TOP_MIDDLE = "┬"
+TOP_RIGHT = "┐"
+LEFT_MIDDLE = "├"
+MIDDLE = "┼"
+RIGHT_MIDDLE = "┤"
+BOTTOM_LEFT = "└"
+BOTTOM_MIDDLE = "┴"
+BOTTOM_RIGHT = "┘"
 
+
+
+
+#if centered: character is ^, else: <
 def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False) -> str:
-    """
-    :param rows: 2D list containing objects that have a single-line representation (via `str`).
-    All rows must be of the same length.
-    :param labels: List containing the column labels. If present, the length must equal to that of each row.
-    :param centered: If the items should be aligned to the center, else they are left aligned.
-    :return: A table representing the rows passed in.
-    """
-    ...
+    # find longest label/element in a column of list, save that number (for all columns, same length of list.list.size) and use it to format the table: {:<10s}{:>4s}   (the number is the number + some lee-way margin)
+    # some number + 2 lee way action
+    longest_elements = []
+
+    return "we got this"
